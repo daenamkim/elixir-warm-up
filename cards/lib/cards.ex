1c5bc3a36b93e13b07619610cd8b636c94af261e
Daenam Kim <daenam.kim@gmail.com>
2019-03-19 05:16:36 +0900

    Practice during udemy

---
+++ cards/lib/cards.ex
@@ -0,0 +1,23 @@
+defmodule Cards do
+  @moduledoc """
+  Documentation for Cards.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Cards.hello()
+      :world
+
+  """
+  def hello do
+    # :world
+    "hi there"
+  end
+
+  def create_deck do
+    ["Ace", "Two", "Three"]
+  end
+end
